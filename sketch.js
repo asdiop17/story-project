c977f162ade5802b8cd44d965e4844a3cad6f634
Abou <Asdiop17@harrisonburg.k12.va.us>
2019-09-16 08:54:13 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,16 +3,16 @@ var veryHungry = false;
 function setup() {
 
   // Change these to change the title and author of the story
-  document.getElementById('title').innerHTML="Goldilocks & the Three Bears"; // title
+  document.getElementById('title').innerHTML="Little Red Riding Hood"; // title
 
-  document.getElementById('author').innerHTML="Jon Stapleton"; // author
+  document.getElementById('author').innerHTML="Abou Diop"; // author
 
 
   
   // All of your code goes under here
   
 
-  var story = 'Goldilocks has been lost in the woods for ' + many() + ' hours. Eventually she discovers a house, and smells something delicious. ' + delicious(); // Create the text
+  var story = 'The Little Red Robin Hood was looking for her way home than she came across a wolf after ' + many() + ' hour(s) of walking through the woods. Eventually the wolf follows her back home. ' + delicious(); // Create the text
 
   printToPage(story); // Print the text to the webpage
 }
@@ -23,15 +23,20 @@ function setup() {
 
 function many() {
   let hours = Math.floor(random(1, 6)); // Random number rounded down
-  if(hours > 3) {
-    veryHungry = true;
-  }
-  return hours
+  if(hours <= 2) {
+    return 'She gets a ride to her grandmas house and gets there safely. The End'
+
+
+  } else if(hours == 3 || hours ==4){
+    return 'She meets the wolf on the way to her grandmas house'
+
+    else if(hours ==5 || hours == 6) }
+  return 'She gets lost after walking through the woods for too long'
 }
 
 function delicious() {
   if(veryHungry == true) {
-    return "Goldilocks is very hungry, so she goes inside."
+    return "Soon after, the wolf ate the grandma before she could make it home. The End."
   } else {
     let inspection = inspects();
     return inspection;
@@ -41,9 +46,9 @@ function delicious() {
 function inspects() {
   let insp = random(1, 6);
   if(insp > 5) {
-    return "Goldilocks sees fresh bear tracks leaving the house!"
+    return "Little Red Riding Hood gets home and kills the grandma thinking that she was the wolf! The End."
   } else {
-    return "Goldilocks inspects the house and everything seems alright. She goes inside."
+    return "Then, the wolf ate the girl and the grandma. The End."
   }
 }
 
